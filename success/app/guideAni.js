eb0660569576de0fb76cd674044dc9e72d3fa6a1
huangnuoen <1261290560@qq.com>
2017-11-20 20:35:25 +0800

    page2 Ani

--- success/app/guideAni.js
+++ success/app/guideAni.js
@@ -3,7 +3,6 @@ const fastclick = require("../lib/fastclick.js");
 const TweenMax = require("../lib/TweenMax.min.js");
 
 
-
 //const guide = $('#guide');
 // page1 Ani
 const logo = $('#guide .page1 .logo')[0];
@@ -17,6 +16,57 @@ TweenLite.from(text1, 2, {
     ease: Power4.easeInOut,
     delay: 1
 });
+
+// page2 Ani
+const circle1 = $('.circle1')[0];
+TweenLite.from(circle1, 0.6, {
+    ease: Elastic.easeOut.config(1.2, 0.4),
+    opacity: 0,
+    scale: 0.3,
+    transformOrigin: 'center center'
+});
+const circle2 = $('.circle2')[0];
+TweenLite.from(circle2, 0.6, {
+    ease: Elastic.easeOut.config(1.2, 0.4),
+    opacity: 0,
+    scale: 0.3,
+    transformOrigin: 'center center'
+});
+const circle3 = $('.circle3')[0];
+TweenLite.from(circle3, 0.3, {
+    ease: Elastic.easeOut.config(1.2, 0.4),
+    scale: 0.1,
+    transformOrigin: 'center center',
+    delay: 0.2
+});
+const logo2 = $('.page2 .logo')[0];
+TweenLite.from(logo2, 1, {
+    ease: Power4.easeInOut,
+    opacity: 0,
+    delay: 0.5
+})
+
+TweenLite.from($('.mini1')[0], 0.8, {
+    ease: Elastic.easeOut.config(1.2, 0.4),
+    opacity: 0,
+    scale: 0,
+    transformOrigin: 'center',
+    delay: 1.5
+})
+TweenLite.from($('.mini2')[0], 1, {
+    ease: Elastic.easeOut.config(1.2, 0.4),
+    scale: 0,
+    transformOrigin: 'center',
+    opacity: 0,
+    delay: 1.8
+})
+TweenLite.from($('.mini3')[0], 1, {
+    ease: Elastic.easeOut.config(1.2, 0.4),
+    opacity: 0,
+    scale: 0,
+    transformOrigin: 'center',
+    delay: 2.1
+})
 
 var touch = {
     current: 1
