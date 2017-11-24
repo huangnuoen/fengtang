685d7e795d450883d292712b248345bb52b03264
unknown <Nuoen.Huang@Fantem.Local>
2017-11-24 15:50:43 +0800

    修改

--- success/app/doneAni.js
+++ success/app/doneAni.js
@@ -1,23 +1,24 @@
-TweenLite.from($('#circle'), 0.8, {
+TweenLite.from($('#circle'), 0.9, {
     ease: Elastic.easeOut.config(1, 1),
     scale: 0,
     transformOrigin: 'center',
     delay: 0.1
 });
-TweenLite.from($('.text')[0], 0.6, {
-    ease: Power4.easeInOut,
-    opacity: 0,
-    delay: 1.2
-});
-TweenLite.from($('.btn')[0], 0.6, {
-    ease: Power4.easeInOut,
-    opacity: 0,
-    delay: 1.2
-});
 TweenLite.from($('#dot'), 0.4, {
+    ease: Power2.easeOut,
     scale: 0,
     transformOrigin: 'center',
-    delay: 1.6
+    delay: 0.5
+});
+TweenLite.from($('.text')[0], 0.7, {
+    ease: Power4.easeInOut,
+    opacity: 0,
+    delay: 1.5
+});
+TweenLite.from($('.btn')[0], 0.7, {
+    ease: Power4.easeInOut,
+    opacity: 0,
+    delay: 1.7
 });
 TweenLite.from($('.add')[0], 1, {
     ease: Elastic.easeOut.config(1, 0.3),
