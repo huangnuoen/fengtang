9d064498103595682acc25f45bcfeeb01bb34fca
unknown <Nuoen.Huang@Fantem.Local>
2017-11-24 11:05:30 +0800

    bling ani

--- success/app/doneAni.js
+++ success/app/doneAni.js
@@ -1,7 +1,8 @@
 TweenLite.from($('#circle'), 0.8, {
     ease: Elastic.easeOut.config(1, 1),
     scale: 0,
-    transformOrigin: 'center'
+    transformOrigin: 'center',
+    delay: 0.1
 });
 TweenLite.from($('.text')[0], 0.6, {
     ease: Power4.easeInOut,
@@ -19,13 +20,18 @@ TweenLite.from($('#dot'), 0.4, {
     delay: 1.6
 });
 TweenLite.from($('.add')[0], 1, {
-    ease: Elastic.easeOut.config(1.2, 0.5),
-    rotation: -180,
+    ease: Elastic.easeOut.config(1, 0.3),
+    rotation: -45,
     transformOrigin: 'center',
     delay: 2
 });
 TweenLite.from($('.add')[1], 1, {
-    ease: Elastic.easeOut.config(1.2, 0.5),
+    ease: Elastic.easeOut.config(1, 0.3),
+    rotation: -90,
+    transformOrigin: 'center',
+    delay: 2
+});TweenLite.from($('.add')[2], 1, {
+    ease: Elastic.easeOut.config(1, 0.3),
     rotation: -180,
     transformOrigin: 'center',
     delay: 2
